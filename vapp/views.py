bbec6fcc42bd0000ca8df4a158361d9dab5342a4
Aaryan Shah <aaryanshah615@gmail.com>
2024-03-23 23:17:38 +0545

    Video_app

--- vapp/views.py
+++ vapp/views.py
@@ -19,17 +19,27 @@ def Login(request):
 
 def signup(request):
     if request.method == "POST":
-        form =RegisterForm(request.POST)
-        if form.is_valid():
-            form.save()
+        email=request.POST['email']
+        first_name=request.POST['first_name']
+        last_name=request.POST['last_name']
+        password1=request.POST.get('password1')
+        password2=request.POST.get('password2')
+        username=email
+        if User.objects.filter(username=username):
+            messages.warning(request,"Username Already Taken")
+            return render(request,'signup.html')
+
+        if password2==password1:      
+            my_user=User.objects.create_user(email=email,username=email,first_name=first_name,last_name=last_name,password=password1)
+            my_user.save()
             messages.success(request,"Account created successfully")
             return redirect('Login')
         else:
-           error=form.errors.as_text()
-           messages.warning(request,error)
-           return redirect('signup')       
+            messages.warning(request,"Please type same password")
+            return redirect('signup')
+        
     return render(request,'signup.html')
-
+    
 def Logout(request):
    logout(request)
    return redirect('Login')
